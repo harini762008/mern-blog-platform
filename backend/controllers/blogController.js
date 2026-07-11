c5993e9a5141a95c8b43d6b970aa26fc077a795d
harini762008 <harinipro25@gmail.com>
2026-07-11 18:09:54 +0530

    Fix blog controller

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,3 +1,6 @@
+const Blog = require("../models/Blog");
+
+// CREATE BLOG
 const createBlog = async (req, res) => {
   try {
     const blog = await Blog.create({
@@ -15,9 +18,179 @@ const createBlog = async (req, res) => {
     res.status(201).json(populatedBlog);
   } catch (err) {
     console.error(err);
-
     res.status(500).json({
       message: err.message,
     });
   }
+};
+
+// GET ALL BLOGS
+const getBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find()
+      .populate("author", "name email")
+      .sort({ createdAt: -1 });
+
+    res.json(blogs);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// GET SINGLE BLOG
+const getBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id)
+      .populate("author", "name email")
+      .populate("comments.user", "name email");
+
+    if (!blog) {
+      return res.status(404).json({
+        message: "Blog not found",
+      });
+    }
+
+    res.json(blog);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// UPDATE BLOG
+const updateBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+
+    if (!blog) {
+      return res.status(404).json({
+        message: "Blog not found",
+      });
+    }
+
+    blog.title = req.body.title || blog.title;
+    blog.content = req.body.content || blog.content;
+
+    if (req.file) {
+      blog.image = req.file.path;
+    }
+
+    await blog.save();
+
+    res.json(blog);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// DELETE BLOG
+const deleteBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+
+    if (!blog) {
+      return res.status(404).json({
+        message: "Blog not found",
+      });
+    }
+
+    await Blog.findByIdAndDelete(req.params.id);
+
+    res.json({
+      message: "Blog deleted successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// ADD COMMENT
+const addComment = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+
+    if (!blog) {
+      return res.status(404).json({
+        message: "Blog not found",
+      });
+    }
+
+    blog.comments.push({
+      user: req.user.id,
+      text: req.body.text,
+    });
+
+    await blog.save();
+
+    const updatedBlog = await Blog.findById(req.params.id)
+      .populate("author", "name email")
+      .populate("comments.user", "name email");
+
+    res.json(updatedBlog);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// LIKE / UNLIKE
+const toggleLike = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+
+    if (!blog) {
+      return res.status(404).json({
+        message: "Blog not found",
+      });
+    }
+
+    const userId = req.user.id;
+
+    const alreadyLiked = blog.likes.some(
+      (id) => id.toString() === userId
+    );
+
+    if (alreadyLiked) {
+      blog.likes = blog.likes.filter(
+        (id) => id.toString() !== userId
+      );
+    } else {
+      blog.likes.push(userId);
+    }
+
+    await blog.save();
+
+    res.json({
+      likesCount: blog.likes.length,
+      liked: !alreadyLiked,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+module.exports = {
+  createBlog,
+  getBlogs,
+  getBlog,
+  updateBlog,
+  deleteBlog,
+  addComment,
+  toggleLike,
 };
